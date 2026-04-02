80047815f298f0a50a26afbf5b2825a5c08620aa
ingroal3005-ship-it <ing.roal3005@gmail.com>
2026-04-01 18:57:02 -0600

    Add files via upload

---
+++ importar_clientes.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prestamos_app.settings')
+django.setup()
+
+from clientes.models import Cliente
+from decimal import Decimal, InvalidOperation
+
+# Leer archivo
+df = pd.read_excel('Prueba_1_importar.xlsx')
+
+def limpiar_decimal(valor):
+    try:
+        if pd.isna(valor):
+            return None
+        valor = str(valor).replace('$', '').replace(',', '').strip()
+        return Decimal(valor)
+    except (InvalidOperation, ValueError):
+        return None
+
+# Recorrer filas
+for i, fila in df.iterrows():
+    try:
+        Cliente.objects.create(
+            nombre=fila['nombre'],      # verifica nombre exacto
+            grupo=fila['grupo'],
+            capital=limpiar_decimal(fila['capital']),  # CORREGIDO
+            interes=int(fila['interes'])
+        )
+    except Exception as e:
+        print(f"Error en fila {i}: {fila}")
+        print(e)
+
+print("Clientes importados correctamente")
+
+# Comando: python importar_clientes.py
